2c613a71722c314380ffd3be60082e256fc3bbc4
Eddie Andress <eddie_andress@hotmail.com>
2020-10-12 12:12:49 +0100

    add stubbing

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -6,4 +6,10 @@ class Calculator
   def multiply(num1, num2)
     num1 * num2
   end
+
+  # this method must use rand
+  # rand is already tested
+  def random_add(num, options = [1,2,3,4,5,6,7,8,9,10])
+    num + options.sample
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,14 +9,25 @@ require 'calculator'
 # expect
   # takes the actual value
   # comapares it to the expected value
+
+# extra structural things
+# cleaning up your test suite
+# try stubbing
+# try doubles
+
+# context
+  # behaves like describe
+  # used to commmunicate the context in which something is happening
+# let / subject
+# stubbing
+  # intercepting a method call
+  # e.g. intercept gets -> return a value (some string)
+
 RSpec.describe Calculator do
 
   describe "#add" do
     it 'returns the total of two numbers' do
-
-      calculator = Calculator.new
-
-      actual_value = calculator.add(1,1)
+      actual_value = subject.add(1,1)
       expected_value = 2
       expect(actual_value).to eq expected_value
     end
@@ -25,14 +36,30 @@ RSpec.describe Calculator do
   describe "#multiply" do
     it "multiplies two numbers" do
 
-      calculator = Calculator.new
-
-      actual_value = calculator.multiply(5,5)
+      actual_value = subject.multiply(5,5)
       expected_value = 25
       expect(actual_value).to eq expected_value
     end
   end
 
-  describe "#divide" do
+  describe "#random_add" do
+    it "takes a number and adds it to a random number" do
+      # don't call rand
+      # don't do anything
+      # just return 3
+      options = [1,2,3,4,5,6,7,8,9,10]
+      allow(options).to receive(:sample).and_return(3)
+      expect(subject.random_add(2, options)).to eq 5
+    end
+  end
+
+  describe "#greeting" do
+    it "selects a greeting from a list, randomly" do
+    end
+  end
+
+  describe "#happy_number?" do
+    it "takes a number and tells you whether or not the number is happy" do
+    end
   end
 end
